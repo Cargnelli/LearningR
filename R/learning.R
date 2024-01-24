5fd8f8ef1bc2858f5d90a4743a30a2f3c0dbb2ad
Cecilie Bøgh Cargnelli <cbc552@gmail.com>
2024-01-24 12:23:23 +0100

    Did a lot of other stuff using dplyr.

--- R/learning.R
+++ R/learning.R
@@ -55,3 +55,75 @@ NHANES %>%
 NHANES %>%
   select(starts_with("bp_")) %>%
   rename(bp_systolic = bp_sys_ave)
+
+
+
+# Filtering data by row ---------------------------------------------------
+
+filter(NHANES, phys_active == "No")
+
+NHANES %>%
+    filter(phys_active == "No")
+
+NHANES %>%
+    filter(phys_active != "No")
+
+NHANES %>%
+    filter(bmi == 25 | phys_active == "No") %>%
+    select(bmi, phys_active)
+
+
+# Arrange rows ------------------------------------------------------------
+
+NHANES %>%
+    arrange(age)
+
+NHANES %>%
+    arrange(education) %>%
+    select(education)
+
+NHANES %>%
+    arrange(desc(age)) %>%
+    select(age)
+
+NHANES %>%
+    arrange(age, education)
+
+
+
+# Mutate ------------------------------------------------------------------
+
+NHANES %>%
+    mutate(old = if_else(age >= 30, "Yes", "No")) %>%
+    select(old)
+
+
+
+# Code section ------------------------------------------------------------
+
+
+# 1. BMI between 20 and 40 with diabetes
+NHANES %>%
+    # Format should follow: variable >= number or character
+    filter(bmi >= 20 & bmi <= 40 & diabetes == "Yes")
+
+# Pipe the data into mutate function and:
+nhanes_modified <- NHANES %>% # Specifying dataset
+    mutate(
+        # 2. Calculate mean arterial pressure
+        mean_arterial_pressure = ((2 * bp_dia_ave) + bp_sys_ave) / 3,
+        # 3. Create young_child variable using a condition
+        young_child = if_else(age < 6, "Yes", "No")
+    )
+
+nhanes_modified
+
+
+
+
+
+
+
+
+
+
